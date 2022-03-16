a156052f8e8b588d5cd2747447cd18c8bf90b2a6
WorldChangerPlus <butsurimori@gmail.com>
2022-03-16 22:29:52 +0900

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import {Typography} from '@material-ui/core'
 
 const App = ()=>{
     return (
         <div>
-            <h1>Hello</h1>
+            <Typography variant="h1">Hello</Typography>
         </div>
     );
 }
