a790a2bd48e88f9ce5b4214c5fe2b269b4d533dc
WorldChangerPlus <butsurimori@gmail.com>
2022-03-24 14:58:12 +0900

    update

--- src/components/Pages/MapPage.jsx
+++ src/components/Pages/MapPage.jsx
@@ -1,11 +1,109 @@
-import React from "react"
-import { Box } from "@mui/material";
+import React from 'react'
+import { Global } from '@emotion/react'
+import { Box, SwipeableDrawer, Typography } from '@mui/material'
+import { ReactComponent as Map } from './map.svg'
+import { grey } from '@mui/material/colors'
 
-const MapPage = () =>{
-    return (
-        <Box>
-            <h1>map</h1>
-        </Box>
-    );
+const drawerBleeding = 90;
+
+class MapPage extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { open: false };
+        this.svg = React.createRef();
+        this.setclick = () => {
+            if (this.svg) {
+                this.svg.current.querySelectorAll('g.prefecture').forEach(element => {
+                    element.onclick = () => {
+                        this.place = element;
+                        this.setState({ open: true });
+                    };
+                }
+                );
+            }
+        }
+        this.place = null;
+    }
+    componentDidMount() {
+        this.setclick();
+    }
+    render() {
+        const toggleDrawer = (newOpen) => {
+            this.setState({ open: newOpen });
+        };
+
+        return (
+            <Box sx={{ position: 'fixed', width: '100%', height: '100%' }}>
+                <style>
+                    {`g.prefecture:hover {
+                        fill:#ff0000;
+                    }`}
+                </style>
+                <Global
+                    styles={{
+                        '.MuiDrawer-root > .MuiPaper-root': {
+                            height: `calc(50% - ${drawerBleeding}px)`,
+                            overflow: 'visible',
+                        },
+                    }}
+                />
+                <Box sx={{ overflow: 'scroll' }}>
+                    <Map ref={this.svg} />
+                </Box>
+                <SwipeableDrawer
+                    open={this.state.open}
+                    anchor='bottom'
+                    onOpen={() => toggleDrawer(true)}
+                    onClose={() => toggleDrawer(false)}
+                    disableSwipeToOpen={false}
+                    swipeAreaWidth={drawerBleeding}
+                    ModalProps={{ keepMounted: true, disableAutoFocus: true }}
+                    BackdropProps={{ invisible: true }}>
+                    <Box sx={{
+                        position: 'absolute',
+                        top: -drawerBleeding,
+                        borderTopRightRadius: 15,
+                        borderTopLeftRadius: 15,
+                        visibility: 'visible',
+                        right: 0,
+                        left: 0,
+                        height: drawerBleeding,
+                        backgroundColor: '#fff',
+                    }}>
+                        <Box sx={{
+                            width: 30,
+                            height: 6,
+                            backgroundColor: grey[300],
+                            borderRadius: 3,
+                            position: 'absolute',
+                            top: 8,
+                            left: 'calc(50% - 15px)',
+                        }} />
+                    </Box>
+                    <Box sx={{ position:'relative',top:-drawerBleeding*2/3,height: '100%', overflowY: 'scroll' }}>
+                        <Typography variant='h4'>物理部</Typography>
+                        <Typography>G-Pointの開発</Typography>
+                        <Typography>G-Pointの開発</Typography>
+                        <Typography>G-Pointの開発</Typography>
+                        <Typography>G-Pointの開発</Typography>
+                        <Typography>G-Pointの開発</Typography>
+                        <Typography>G-Pointの開発</Typography>
+                        <Typography>G-Pointの開発</Typography>
+                        <Typography>G-Pointの開発</Typography>
+                        <Typography>G-Pointの開発</Typography>
+                        <Typography>G-Pointの開発</Typography>
+                        <Typography>G-Pointの開発</Typography>
+                        <Typography>G-Pointの開発</Typography>
+                        <Typography>G-Pointの開発</Typography>
+                        <Typography>G-Pointの開発</Typography>
+                        <Typography>G-Pointの開発</Typography>
+                        <Typography>G-Pointの開発</Typography>
+                        <Typography>G-Pointの開発</Typography>
+                    </Box>
+                </SwipeableDrawer>
+            </Box>
+        )
+    }
 }
+
 export default MapPage
--- src/components/Templates/Footer.jsx
+++ src/components/Templates/Footer.jsx
@@ -9,7 +9,7 @@ import MessageIcon from '@mui/icons-material/Message';
 import { Link } from "react-router-dom"; 
 
 const PageIndex={
-    "":0,"map":1,"schedule":2,"alert":3
+    "home":0,"map":1,"schedule":2,"alert":3
 };
 
 const Footer =()=>{
@@ -19,8 +19,10 @@ const Footer =()=>{
     return (
         <Box sx={{
             position:'fixed',
-            bottom:0,
-            width: "100%",
+            bottom:'env(safe-area-inset-bottom)',
+            left:0,
+            width: '100%',
+            zIndex:1201
         }}>
             <BottomNavigation
                 showLabels
@@ -29,7 +31,7 @@ const Footer =()=>{
                     setValue(newValue);
                 }}
             >
-                <BottomNavigationAction label="ホーム" icon={<HomeIcon />} component={Link} to='/'/>
+                <BottomNavigationAction label="ホーム" icon={<HomeIcon />} component={Link} to='/home'/>
                 <BottomNavigationAction label="校内マップ" icon={<MapIcon />} component={Link} to='/map'/>
                 <BottomNavigationAction label="スケジュール" icon={<DateRangeIcon />} component={Link} to='/schedule'/>
                 <BottomNavigationAction label="お知らせ" icon={<MessageIcon />} component={Link} to='/alert'/>
@@ -40,4 +42,3 @@ const Footer =()=>{
 }
 
 export default Footer
-
